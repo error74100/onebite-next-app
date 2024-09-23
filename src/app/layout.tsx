4d539b7eec0f76c4e6559c099ff507cb966e088f
error7410 <error7410@gmail.com>
2024-09-23 11:21:25 +0900

    섹션 9. 고급 라우팅 패턴

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import Link from 'next/link';
 import style from './layout.module.css';
 import { BookData } from '@/types';
+import { ReactNode } from 'react';
 
 async function Footer() {
   const response = await fetch(
@@ -26,11 +27,13 @@ async function Footer() {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body>
         <div className={style.container}>
           <header>
@@ -39,6 +42,9 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </div>
+
+        {modal}
+        <div id="modal-root"></div>
       </body>
     </html>
   );
